49ccc58d20671a91e7857c32b13a9b5bb623f75a
Islam Ibrahim <99902@zhic.gov.ae>
2022-01-23 15:19:34 +0400

    remove migration

--- Inventory.API/Startup.cs
+++ Inventory.API/Startup.cs
@@ -18,7 +18,6 @@ namespace Inventory.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services
@@ -36,7 +35,6 @@ namespace Inventory.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
